7aec74803749b8e7a07345b501084a034d8386f7
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-09 18:17:43 +0100

    Update App.js
    added in style sheet path

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './assests/css/style.css';
 import Header from './Header';
 import Home from './Home';
 import Projects from './Projects';
