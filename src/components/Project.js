3310f9e5cc371392eadcd4a13c32307a9059bc40
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-09 18:27:44 +0100

    Update Project.js
    added in style sheet

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './assests/css/style.css';
 
 function Project({ projectsData }) {
   const { projectId } = useParams();
