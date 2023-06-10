5edb78cb1bf4e9f005ae513166219add85def1be
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-06-10 17:21:47 +0100

    Update Home.js
    added </p> tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ function Home() {
           className="avatar"
         />
         <h3>Hello, I'm Ayshah Malik</h3>
-        <p>A passionate Computer Science student with a keen interest in web development and user experience (UX).
+        <p>A passionate Computer Science student with a keen interest in web development and user experience (UX).</p>
         I am dedicated to creating intuitive and user-friendly web experiences that combine my technical skills with a strong focus on user needs and aesthetics.
         With a methodical approach to problem-solving and a creative mindset, I strive to create visually appealing and engaging websites that provide a seamless user experience.</p>
         <p>I am excited to showcase my projects and share my passion for web development and UX through this portfolio.</p>
